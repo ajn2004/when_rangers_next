bd483f43f1012edbf8e5843797a3845d08e58d9a
Andrew Nelson <andrew@daleego.com>
2024-12-27 22:11:08 -0500

    coercing error message into string

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,7 +1,10 @@
 // src/components/Error.js
 import React from 'react';
 
-const Error = ({ message }) => <p>Error: {String(message)}</p>;
+const Error = ({ message }) => {
+    const errorMessage = typeof message === 'string' ? message : JSON.stringify(message);
+    <p>Error: {String(message)}</p>
+};
 
 export default Error;
 
